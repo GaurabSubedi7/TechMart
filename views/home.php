c3f5078b4e4302b475a38f03275df45584a0e5ed
Gaurav Subedi <53326558+NightHawk777@users.noreply.github.com>
2020-09-21 21:56:39 +0545

    Update home.php

--- views/home.php
+++ views/home.php
@@ -46,10 +46,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <script>
-        $(document).ready(function(){
-            $('.sidenav').sidenav();
-        });
-    </script>
+    <script src="./views/public/scripts/home.js"></script>
 </body>
 </html>
