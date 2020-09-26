fb1a2ba9a41abb3068c7a921547f7c0575988977
Gaurav Subedi <53326558+NightHawk777@users.noreply.github.com>
2020-09-26 13:59:00 +0545

    404 page updated

--- views/notfound.php
+++ views/notfound.php
@@ -14,8 +14,17 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <title>404 not found</title>
+    <style>
+        body{
+            background: url("./views/public/img/notfound.png");
+            background-position: center;
+        }
+    </style>
 </head>
 <body>
-    <h1><?php echo $invalidroute; ?> not found</h1>
+    <div class="container">
+        <h1>404 Error</h1>
+        <h3>Page Not Found</h3>
+    </div>
 </body>
 </html>
